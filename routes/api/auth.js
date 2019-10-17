6878abb2181ab9b7b839954c8c6acd78cfcdbff9
AlfredoE89 <w0lf_0f_wall_street@icloud.com>
2019-10-16 19:54:12 -0500

    Add location.hash detection for app navigation

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -66,4 +66,9 @@ router.delete('/logout', function(req, res){
   return res.json({success: 'false'});
 });
 
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/auth');
+});
+
 module.exports = router;
