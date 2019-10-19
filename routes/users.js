9f01791c37276f7d0e8edf1a0d42ed6ccb8b45c7
AlfredoE89 <w0lf_0f_wall_street@icloud.com>
2019-10-19 16:21:58 -0500

    Add a non-API endpoint for accessing the user app

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var Users = require('../models/users');
 
-// router.get('/', function(req, res) {
-//   res.send('respond with a resource');
-// });
-
-router.get('/', function(req, res, next) {
-
-  Users.find({},function(err, users){
-    if(err){
-     return res.json({'success':false, 'error': err});
-    }
-
-    return res.json({'success':true, 'users': users});
-  });
-
+router.get('/app', function(req, res, next) {
+  res.render('users/app', { title: 'User Management' });
 });
 
 module.exports = router;
