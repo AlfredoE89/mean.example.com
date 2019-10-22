8c48e96ab8b2c7af85098a46ed15aa0723452fd0
AlfredoE89 <w0lf_0f_wall_street@icloud.com>
2019-10-22 18:07:02 -0500

    Add a non-API endpoint for accessing the user app

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,18 @@ gulp.task('build-main-js', function() {
   return merge(authApp);
 });
 
+gulp.task('build-users-js', function() {
+
+  var userApp = gulp.src([
+    'src/js/users.app.js',
+  ])
+  .pipe(concat('users.app.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('public/dist/js'));
+
+  return merge(userApp);
+});
+
 gulp.task('build-auth-js', function() {
 
   var authApp = gulp.src([
@@ -61,4 +73,5 @@ gulp.task('build-css', gulp.series('build-main-css'));
 gulp.task('build-js', gulp.series(
   'build-main-js',
   'build-auth-js',
+  'build-users-js'
 ));
