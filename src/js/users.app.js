97bdf0da5ba15497c6daee80a3750bcc0fdfd5ef
AlfredoE89 <w0lf_0f_wall_street@icloud.com>
2019-10-23 17:05:53 -0500

    articles

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -279,7 +279,7 @@ var usersApp = (function() {
           <div>Email: <strong>${data.user.email}</strong></div>
           <div class="text-center">
             <br>
-            <a class="btn btn-lg btn-danger text-white">
+            <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
               Yes delete ${data.user.username}
             </a>
           </div>
@@ -288,6 +288,31 @@ var usersApp = (function() {
 
       app.innerHTML = card;
     }
+  }
+
+  function deleteUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      if(data.success === true){
+        window.location.hash = '#';
+      }else{
+        alert('Unknown error, the user could not be deleted');
+      }
+
+    }
+
   }
 
   return {
@@ -317,9 +342,17 @@ var usersApp = (function() {
           viewUsers();
           break;
       }
+    },
+
+    deleteUser: function(id){
+      deleteUser(id);
     }
-  }
-    
-})();
+
+    }
+    })();
     
     usersApp.load();
+
+    window.addEventListener("hashchange", function(){
+      usersApp.load();
+    });
